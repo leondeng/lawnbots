909748d92ee4c1bbe3ed8bff3ea2e3e03734205e
Fan Deng <fan.deng@momentumcloud.com.au>
2015-05-22 09:33:02 +1000

    bot: express self

--- lib/Fan/Lawnbots/Entity/Bot.php
+++ lib/Fan/Lawnbots/Entity/Bot.php
@@ -11,74 +11,78 @@ use Doctrine\ORM\Mapping as ORM;
 class Bot extends BaseEntity
 {
   use Traits\Accessor;
-  
+
   private $x;
-  
+
   private $y;
-  
+
   private $heading;
-  
+
   private $command;
-  
+
   public function __construct($position) {
     $params = explode(' ', $position);
     if (count($params) !== 3) {
      throw new \InvalidArgumentException('Wrong position string!');
     }
-    
+
     $this->initialize($params);
   }
-  
+
   private function initialize(array $params) {
     $this->setX($params[0]);
     $this->setY($params[1]);
     $this->setHeading($params[2]);
   }
-  
+
   public static function create($postion, $command) {
     $bot = new Bot($postion);
     $bot->setCommand($command);
-    
+
     return $bot;
   }
-  
+
   public function setX($x) {
     if (!is_numeric($x)) {
       throw new \InvalidArgumentException('Wrong x position!');
     }
-    
+
     $this->x = (int) $x;
-    
+
     return $this;
   }
-  
+
   public function setY($y) {
     if (!is_numeric($y)) {
       throw new \InvalidArgumentException('Wrong y position!');
     }
-    
+
     $this->y = (int) $y;
-    
+
     return $this;
   }
-  
+
   public function setHeading($heading) {
     if (!in_array($heading, array('N', 'S', 'E', 'W'))) {
       throw new \InvalidArgumentException('Wrong heading!');
     }
-    
+
     $this->heading = $heading;
-    
+
     return $this;
   }
-  
+
   public function setCommand($command) {
     if (!preg_match('/^[LRM]+$/', $command)) {
       throw new \InvalidArgumentException('Wrong command string!');
     }
-    
+
     $this->command = $command;
-    
+
     return $this;
+  }
+
+  public function __toString() {
+    return sprintf('%d %d %s', $this->x, $this->y, $this->heading);
   }
 }
--- tests/Fan/Lawnbots/Tests/unit/BotTest.php
+++ tests/Fan/Lawnbots/Tests/unit/BotTest.php
@@ -7,40 +7,50 @@ use Fan\Lawnbots\Entity\Bot;
 class BotTest extends \PHPUnit_Framework_TestCase
 {
 
-  public function testBotCreate() {
+  public function testCreate() {
     $bot = $this->getBot();
     $this->assertInstanceOf('Fan\Lawnbots\Entity\Bot', $bot);
     $this->assertEquals(1, $bot->getX());
     $this->assertEquals(2, $bot->getY());
     $this->assertEquals('LMLMLMLMM', $bot->getCommand());
   }
-  
-  public function testBotCreateWrongPosition() {
+
+  public function testCreateWrongPosition() {
     $this->setExpectedException('InvalidArgumentException', 'Wrong position string!');
     $bot = Bot::create('11N', 'S62d8');
   }
-  
-  public function testBotCreateWrongCommand() {
+
+  public function testCreateWrongCommand() {
     $this->setExpectedException('InvalidArgumentException', 'Wrong command string!');
     $bot = Bot::create('2 3 S', 'sjfewpnfe21');
   }
-  
-  public function testBotCreateWrongXPostion() {
+
+  public function testCreateWrongXPostion() {
     $this->setExpectedException('InvalidArgumentException', 'Wrong x position!');
     $bot = $this->getBot();
     $bot->setX('x');
   }
-  
-  public function testBotCreateWrongPostion() {
+
+  public function testCreateWrongPostion() {
     $this->setExpectedException('InvalidArgumentException', 'Wrong y position!');
     $bot = $this->getBot();
     $bot->setY('y');
   }
-  
-  public function testBotCreateWrongHeading() {
+
+  public function testCreateWrongHeading() {
     $this->setExpectedException('InvalidArgumentException', 'Wrong heading!');
     $bot = $this->getBot();
     $bot->setHeading('999');
+  }
+
+  public function testtoString() {
+    $bot = $this->getBot();
+    $this->assertEquals('1 2 N', (string) $bot);
+
+    $bot->setX(3);
+    $bot->setY(5);
+    $bot->setHeading('W');
+    $this->assertEquals('3 5 W', (string) $bot);
   }
 
   public function getBot() {
@@ -48,6 +58,6 @@ class BotTest extends \PHPUnit_Framework_TestCase
 
     return $bot;
   }
-  
-  
+
+
 }
